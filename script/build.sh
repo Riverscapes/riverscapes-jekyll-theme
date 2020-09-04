adefa825950b27aff7cc522e240b13b5dffecf37
Matt Reimer <matt.reimer@gmail.com>
2020-09-04 08:45:18 -0700

    precompiling all the css fixes #16

--- script/build.sh
+++ script/build.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-echo "Building the example site..."
-bundle exec jekyll build
+# echo "Building the example site..."
+# bundle exec jekyll build
+sass _sass/app.scss assets/css/style.css -t compressed
